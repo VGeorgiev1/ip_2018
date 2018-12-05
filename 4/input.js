3581cfa769a29f91bcb72bf4907d38dc2cdc1e85
Vladislav <vladdislav.georgiev@gmail.com>
2018-12-05 19:23:31 +0200

    4th homework done

---
+++ 4/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /Internet|Words|Or/,
+"exercise_2": /088[^80-]{7}/,
+"exercise_3": /[^01]+/,
+"exercise_4": /^[a-z][^@$]{2,17}$/,
+"exercise_5": /^[0-9][0-9][0-9]$|^1(?:(?=5)50{2}|[0-4][0-9]+)$/,
+"exercise_6": /class=('|")[A-z\s]+('|")/
+};
+cssSelectors = {
+"exercise_1": "[name='3-2']",
+"exercise_2": ".diffClass",
+"exercise_3": "[name='3-2-3']",
+"exercise_4": "[name='3']",
+"exercise_5": "[name='1-1-2']",
+"exercise_6": "item.class2 > item, item.class1 > item",
+"exercise_7": "[name='4-2-2']",
+"exercise_8": "[name='2']"
+};
